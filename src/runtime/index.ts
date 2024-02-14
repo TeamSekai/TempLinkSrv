1f14d93dce4773b07b0eafaff156e83f1b8f3066
takejohn <105504345+takejohn@users.noreply.github.com>
2024-02-14 16:22:32 +0900

    ルートのhtmlファイルを作成

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -1,12 +1,15 @@
 // @deno-types="npm:@types/express@4.17.21"
 import express from 'npm:express@4.18.2';
+import { fromFileUrl } from "https://deno.land/std@0.215.0/path/mod.ts";
 
 import { CONFIG } from '../setup/index.ts';
 
 const app = express();
 
+const rootHtmlPath = fromFileUrl(new URL('../resources/html/index.html', import.meta.url));
+
 app.get('/', (_req, res) => {
-    res.send('Hello, world!');
+    res.sendFile(rootHtmlPath);
 });
 
 app.listen(CONFIG.linkPort, CONFIG.linkHostname);
