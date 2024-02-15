f12e6ff7ba321497d66c0991c5670aafb4ec4aa3
takejohn <105504345+takejohn@users.noreply.github.com>
2024-02-15 20:19:10 +0900

    Promise.resolveがあるんだった

--- src/runtime/database/SQLiteStorage.ts
+++ src/runtime/database/SQLiteStorage.ts
@@ -2,7 +2,6 @@ import { LinkRecord } from './LinkRecord.ts';
 import { DataStorage } from './DataStorage.ts';
 import { DB } from 'https://deno.land/x/sqlite@v3.8/mod.ts';
 import { fromFileUrl } from 'https://deno.land/std@0.215.0/path/mod.ts';
-import { synchronizedPromise } from '../util/promises.ts';
 
 export class SQLiteStorage implements DataStorage {
     private readonly database = new DB(fromFileUrl(new URL('../../../storage.db', import.meta.url)));
@@ -16,60 +15,53 @@ export class SQLiteStorage implements DataStorage {
         );`);
     }
 
-    insertLink = (id: string, record: LinkRecord) => {
-        return synchronizedPromise(() => {
-            if (this.database.query('SELECT id FROM links WHERE id = ?;', [id]).length != 0) {
-                return false;
-            }
-            this.database.query('INSERT INTO links VALUES (?, ?, ?, ?);', [
-                id,
-                record.destination.toString(),
-                record.expirationTime,
-                record.creationDate,
-            ]);
-            return true;
-        });
+    insertLink(id: string, record: LinkRecord) {
+        if (this.database.query('SELECT id FROM links WHERE id = ?;', [id]).length != 0) {
+            return Promise.resolve(false);
+        }
+        this.database.query('INSERT INTO links VALUES (?, ?, ?, ?);', [
+            id,
+            record.destination.toString(),
+            record.expirationTime,
+            record.creationDate,
+        ]);
+        return Promise.resolve(true);
     };
 
-    selectLinkById = (id: string) => {
-        return synchronizedPromise(() => {
-            const result = this.database.query<[string, number, number]>(
-                'SELECT destination, expiration_time, creation_date FROM links WHERE id = ?;',
-                [id],
-            );
-            if (result.length == 0) {
-                return null;
-            }
-            const [destination, expirationTime, creationDate] = result[0];
-            return new LinkRecord(new URL(destination), expirationTime, creationDate);
-        });
+    selectLinkById (id: string) {
+        const result = this.database.query<[string, number, number]>(
+            'SELECT destination, expiration_time, creation_date FROM links WHERE id = ?;',
+            [id],
+        );
+        if (result.length == 0) {
+            return Promise.resolve(null);
+        }
+        const [destination, expirationTime, creationDate] = result[0];
+        return Promise.resolve(new LinkRecord(new URL(destination), expirationTime, creationDate));
     };
 
-    selectLinksByExpirationDate = (expirationDate: number) => {
-        return synchronizedPromise(() => {
-            const result = this.database.query<[string, string, number, number]>(
-                'SELECT id, destination, expiration_time, creation_date FROM links WHERE creation_date + expiration_time <= ?',
-                [expirationDate],
-            );
-            const map = new Map<string, LinkRecord>();
-            for (const [id, destination, expirationTime, creationDate] of result) {
-                map.set(id, new LinkRecord(new URL(destination), expirationTime, creationDate));
-            }
-            return map;
-        });
+    selectLinksByExpirationDate(expirationDate: number) {
+        const result = this.database.query<[string, string, number, number]>(
+            'SELECT id, destination, expiration_time, creation_date FROM links WHERE creation_date + expiration_time <= ?',
+            [expirationDate],
+        );
+        const map = new Map<string, LinkRecord>();
+        for (const [id, destination, expirationTime, creationDate] of result) {
+            map.set(id, new LinkRecord(new URL(destination), expirationTime, creationDate));
+        }
+        return Promise.resolve(map);
     };
 
-    deleteLink = (id: string) => {
-        return synchronizedPromise(() => {
-            if (this.database.query('SELECT id FROM links WHERE id = ?;', [id]).length == 0) {
-                return false;
-            }
-            this.database.query('DELETE FROM links WHERE id = ?;', [id]);
-            return true;
-        });
+    deleteLink(id: string) {
+        if (this.database.query('SELECT id FROM links WHERE id = ?;', [id]).length == 0) {
+            return Promise.resolve(false);
+        }
+        this.database.query('DELETE FROM links WHERE id = ?;', [id]);
+        return Promise.resolve(true);
     };
 
-    close = () => {
-        return synchronizedPromise(() => this.database.close(true));
+    close() {
+        this.database.close();
+        return Promise.resolve();
     };
 }
--- src/runtime/util/promises.ts
+++
@@ -1,9 +0,0 @@
-export function synchronizedPromise<T>(func: () => T): Promise<T> {
-    return new Promise((resolve, reject) => {
-        try {
-            resolve(func());
-        } catch (e) {
-            reject(e);
-        }
-    });
-}
